446baa1a3a17a1b47f0633ee6d8f7e5969c07911
Muhammad Qasim <154578365+mqasimkh@users.noreply.github.com>
2025-06-23 10:05:53 +0500

    interface

--- code/intf.sv
+++ code/intf.sv
@@ -1,7 +1,7 @@
-interface intf (input clk);
+interface intf #(DATA_WIDTH = 8)(input clk);
     logic rst_n;
     logic wr_en, rd_en;
-    logic [7:0] data_in;
-    logic [7:0] data_out;
+    logic [DATA_WIDTH-1:0] data_in;
+    logic [DATA_WIDTH-1:0] data_out;
     logic full, empty;
 endinterface
