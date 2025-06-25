156cd22cde21df943f04c7880ddcff632d955d22
Muhammad Qasim <154578365+mqasimkh@users.noreply.github.com>
2025-06-25 06:05:31 +0500

    moving test generator

--- code/generator.sv
+++ code/generator.sv
@@ -14,22 +14,39 @@ class generator;
         t = new();
     endfunction: new
 
-    // task run();
-    //     bit ok;
-    //     repeat (count) begin
-    //         transaction temp;
-    //         ok = t.randomize();
-    //         assert (ok) else $error("Randomization Failed");
-    //         temp = t.clone();
-    //         gen2drv.put(temp);
-    //         gen2scr.put(temp);
-    //         gen_count++;
-    //         t.display();
-    //     end
-    //     -> complete;
-    // endtask: run
+    /////////////////////////////////
+    /////////    Main Run   /////////
+    /////////////////////////////////
 
-        task run();
+
+    task run();
+        read_after_write();
+        -> complete;
+    endtask: run
+
+
+    /////////////////////////////////
+    /////////    Tests      /////////
+    /////////////////////////////////
+
+    task write_full();
+        bit ok;
+        repeat (count) begin
+            transaction temp;
+            t.wr_en.rand_mode(0);
+            t.rd_en.rand_mode(0);
+            t.wr_en = 1;
+            t.rd_en = 0;
+            assert (ok) else $error("Randomization Failed");
+            gen2drv.put(temp);
+            gen2scr.put(temp);
+            t.display();
+            gen_count++;
+        end
+    endtask: write_full
+
+
+    task read_after_write();
         bit ok;
         repeat (count/2) begin
             transaction temp;
@@ -63,9 +80,6 @@ class generator;
             t.display();
             gen_count++;
         end
-
-
-        -> complete;
-    endtask: run
+    endtask: read_after_write
 
 endclass: generator
